078509f0626ca8f91e7224d5e6011405ad7d42fc
FeHeap <82155803+FeHeap@users.noreply.github.com>
2021-08-01 14:05:13 +0800

    simple sub cipher & affine cipher
    encrypt, decrypt, hacker about simple sub cipher
    hacker about affine cipher and debug about affine cipher decrypt

--- cryptography/affineCipher.py
+++ cryptography/affineCipher.py
@@ -6,9 +6,7 @@ import sys, pyperclip, cryptomath, random
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
 def main():
-    myMessage = """"A computer would deserve to be called intelligent
-if it could deceive a human into believing that it was human."
--Alan Turing"""
+    myMessage = """"A computer would deserve to be called intelligent if it could deceive a human into believing that it was human." -Alan Turing"""
 
     myKey = 2894
     myMode = 'encrypt' #may be 'encrypt' or 'decrypt'
@@ -33,7 +31,7 @@ def getKeyParts(key):
 def checkKeys(keyA, keyB, mode):
     if keyA == 1 and mode == 'encrypt':
         sys.exit('Cipher is weak if key A is 1. Choose a different key.')
-    if keyB == 0 and mode == 'decrypt':
+    if keyB == 0 and mode == 'encrypt':
         sys.exit('Cipher is weak if key B is 0. Choose a different key.')
     if keyA < 0 or keyB < 0 or keyB > len(SYMBOLS) - 1:
         sys.exit('Key A must be greater than 0 and Key B must be between 0 and %s.'%(len(SYMBOLS)-1))
